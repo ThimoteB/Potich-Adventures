62add23c8e4389854498bc4461d0806db7002c18
Colin <44371364+Colnup@users.noreply.github.com>
2024-06-14 08:45:22 +0200

    fix: lower regression probability (#45)

--- server_classes/goal/cardGoal.py
+++ server_classes/goal/cardGoal.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 from server_classes.goal.goal import Goal
 
+
 class cardGoal(Goal):
     def __init__(self):
-        super().__init__(5,0.5)
+        super().__init__(5, 0.05)
         self.name = "cardGoal"
-    
--- server_classes/goal/playerGoal.py
+++ server_classes/goal/playerGoal.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 from server_classes.goal.goal import Goal
 
+
 class playerGoal(Goal):
     def __init__(self):
-        super().__init__(10,0.5)
+        super().__init__(10, 0.01)
         self.name = "playerGoal"
-        
